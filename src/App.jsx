cc2f774c0284a9a477e13a9b2c5fe4437c105f8a
HassanAbukar <xasaani.abukar@example.com>
2026-06-26 01:29:21 -0700

    added exercise 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 //import UserCard from "./UserCard";
-import Blog from "./Blog";
+//import Blog from "./Blog";
+import UserCard from "./UserCard";
 
 const App = () =>{
     return (
         <div>
-            <Blog />
+           <UserCard name = "Hassan Abukar" email = "hassan@gmail.com" password = "1234" />
+           <UserCard name = "Ali Abukar" email = "ali@gmail.com" password = "32432" />
+           <UserCard name = "Ak" email = "ak@gmail.com" password = "312321" />
         </div>
+
+        
     );
 }
 
--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,10 +1,16 @@
-const UserCard = () => {
+const UserCard = ({name,email,password}) => {
 
     return (
-        <div>
-            <h2>John Doe</h2>
-            <p>john.doe@example.com</p>
-        </div>
+        // <div>
+        //     <h2>John Doe</h2>
+        //     <p>john.doe@example.com</p>
+        // </div>
+
+        <>
+        <h2>name, {name}</h2>
+        <h3>email, {email}</h3>
+        <h3>password, {password}</h3>
+        </>
     );
 }
 
